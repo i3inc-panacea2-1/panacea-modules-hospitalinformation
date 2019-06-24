ac1950ce75456d7521fc11ff10fab416c917d3e3
Portno <j.remoundos@dotbydot.gr>
2019-06-24 13:02:17 +0300

    nuget + text size

---
+++ src/Panacea.Modules.HospitalInformation/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+
+namespace Panacea.Modules.HospitalInformation
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+
+        public App()
+        {
+
+        }
+    }
+}
+
